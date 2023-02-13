1187f64254a32dafd386a09d50fd43fb83d4db8a
tanvir <tanvirnovustqf@gmail.com>
2023-02-14 00:56:32 +0600

    Banner adding

---
+++ src/components/Banner.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Electrical Engineer", "Software Engineer", "QA Engineer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Tanvir`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Electrical Engineer", "Software Engineer", "QA Engineer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Currently working as a Software Quality Assurance Engineer at TigerIT Bangladesh Ltd.
+Previously worked as a Maintenance Engineer in Beximco Group. Successfully completed a world-class medical, testing, and health screening facility project of Beximco Group (A joint venture project of Beximco Group and Fullerton Health Singapore). Developed an automatic room assigning software, a store inventory management software using MS Excel- Macro, VBA etc.
+Completed Bachelor of Science (BSc) in EECE from Military Institute of Science and Technology (MIST) & was involved in several extra-curricular & volunteer activities for personal & professional growth.
+Intended to be a result-oriented professional driven by the desire to excel business by utilizing new concepts, along with expertise in Technical Design, Operation, Project Management, Automation, Process Development, Risk Management as well as Technical planning & implementation.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
