e0e1b9a72b5180d32d0d3147ad91b151c4decaff
tanvir <tanvirnovustqf@gmail.com>
2023-02-21 23:28:21 +0600

    education and experience added

---
+++ src/components/Experience.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Section from "../components/Section";
+
+export default function Experience() {
+  return (
+    <Section id="experience" title="Experience">
+      <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div className="flex-grow-1">
+          <h3 className="mb-0">Flutter Developer Intern</h3>
+          <div className="subheading mb-3">Skillablers ~ Crafting Careers...Skillfully!</div>
+          <p>
+            <strong>Roles/Responsibilities</strong><br />
+            <li>Responsible to handle the Frontend as well as Backend.</li>
+            <li>Implement new features to the apps.</li>
+            <li>Make the app very efficient.</li>
+            <li>Implement many alternative solution for a problem.</li>
+          </p>
+        </div>
+        <div className="flex-shrink-0">
+          <span className="text-primary">June 2021 - Present</span>
+        </div>
+      </div>
+      <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div className="flex-grow-1">
+          <h3 className="mb-0">Maintenance Engineer</h3>
+          <div className="subheading mb-3">Beximco Group</div>
+          <p>
+            <li>1. Ensuring safety for the electrical equipment- transformer, generator, water pump, etc.</li>
+            <li>2. Preparing SOP for the maintenance of the elevator and maintaining it.</li>
+            <li>3. Operation, Maintenance and Troubleshooting of utility systems.</li>
+            <li>4. Manpower planning & task planning for the execution of project work.</li>
+            <li>5. Dealing with vendors and preparing required BOQ.</li>
+            <li>6. Executing electrical installation work according to the drawing.</li>
+            <li>7. Identifying the problem in the project and developing a better solution.</li>
+            <li>8. Maintaining the store inventory and spares and financial planning for the operational expenditure.</li>
+            <li>9. Developed an Automated Room Assigning Software using VBA code and Macro in Microsoft Excel.</li>
+            <li>10. Developed a Store Inventory Management Software using Microsoft Excel (Macro and VBA Code).</li>
+            <li>11. Developed an Automated Invoice Software for Accounts Department..</li>
+            </p>
+        </div>
+        <div className="flex-shrink-0">
+          <span className="text-primary">June 2021 - Present</span>
+        </div>
+      </div>
+      <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div className="flex-grow-1">
+          <h3 className="mb-0">Lead</h3>
+          <div className="subheading mb-3">Microsoft Student Ambassadors Community KIIT</div>
+          <p>
+            <li>In-charge of App Dev, Web Dev & ML Team.</li>
+            <li>I do projects along with my team.</li>
+            <li>Responsible to take core decisions for MSAC.</li>
+            <li>Organize College, National and International level events.</li>
+          </p>
+        </div>
+        <div className="flex-shrink-0">
+          <span className="text-primary">July 2020 - Present</span>
+        </div>
+      </div>
+    </Section>
+  );
+}
